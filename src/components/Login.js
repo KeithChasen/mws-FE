9370af2d142ae0d848531af9d91cfca6caaaf804
keithchasen <keithchasen89@gmail.com>
2021-05-21 18:48:41 +0300

    login functionality

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useMutation } from "@apollo/client";
 
-const Login = () => (
-  <div>Login</div>
-);
+import { LOGIN } from '../graphql/auth';
+
+const Login = () => {
+  const history = useHistory();
+
+  const [credentials, setCredentials] = useState({
+    email: '',
+    password: ''
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const errorStyle = { border: '1px solid red'};
+
+  const [login, { data }] = useMutation(LOGIN);
+
+  const onChange = e => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const submitForm = e => {
+    e.preventDefault();
+    login({ variables: credentials })
+      .then(res => {
+        if (res.data.login.token) {
+          localStorage.setItem('token', res.data.login.token);
+          history.push('/');
+        } else {
+          setErrors({
+            ...errors,
+            common: 'Something went wrong'
+          })
+        }
+      })
+      .catch(errors => {
+        setErrors({
+          ...errors.graphQLErrors[0].extensions.errors
+        })
+      })
+  };
+
+  return (
+    <div>
+      <h1>
+        Login
+      </h1>
+      <form onSubmit={submitForm}>
+        <input type="email" name='email' style={errors.email ? errorStyle : null} onChange={onChange}/>
+        <p style={{color: 'red'}}>{errors.email}</p>
+        <input type="password" name='password' style={errors.password ? errorStyle : null} onChange={onChange}/>
+        <p style={{color: 'red'}}>{errors.password}</p>
+        <button>Login</button>
+        <p style={{color: 'red'}}>{errors.common}</p>
+      </form>
+    </div>
+  );
+}
 
 export default Login;
--- src/graphql/auth.js
+++ src/graphql/auth.js
@@ -20,6 +20,23 @@ const REGISTER = gql`
   }
 `;
 
+const LOGIN = gql`
+  mutation login(
+      $email: String!
+      $password: String!
+  ) {
+      login(
+          loginInput: {
+              email: $email
+              password: $password
+          }
+      ) {
+          id
+          token
+      }
+  }
+`;
+
 export {
-  REGISTER
+  REGISTER, LOGIN
 };
