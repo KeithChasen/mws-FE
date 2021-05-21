36b84bfa0541b4afa9ad112997c5afb20935e48f
keithchasen <keithchasen89@gmail.com>
2021-05-21 13:33:24 +0300

    menu style and active style

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const Menu = () => (
-  <nav>
-    <ul>
-      <NavLink exact to="/">Home</NavLink>
-      <NavLink to="/login">Login</NavLink>
-      <NavLink to="/register">Register</NavLink>
-    </ul>
-  </nav>
-);
+const Menu = () => {
+  const activeStyle = { background: 'grey', color: 'black' };
+  const styles = { margin: '10px' };
+
+  return (<nav>
+      <ul>
+        <NavLink exact to="/" activeStyle={activeStyle} style={styles}>Home</NavLink>
+        <NavLink to="/login" activeStyle={activeStyle} style={styles}>Login</NavLink>
+        <NavLink to="/register" activeStyle={activeStyle} style={styles}>Register</NavLink>
+      </ul>
+    </nav>
+  )
+};
 
 export default Menu;
