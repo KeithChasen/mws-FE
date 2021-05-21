ddd83b3afa7faba7ea1e3a503b13966fa40dc182
keithchasen <keithchasen89@gmail.com>
2021-05-21 13:20:20 +0300

    basic login and register routes and components

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import { BrowserRouter, Route } from 'react-router-dom'
 
 import './App.css';
 import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
   return (
     <BrowserRouter>
       <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
     </BrowserRouter>
   );
 }
