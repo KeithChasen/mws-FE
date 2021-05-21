95270f0eb09f46f390fbde254dcbe0bc9c73118d
keithchasen <keithchasen89@gmail.com>
2021-05-21 14:37:03 +0300

    basic register form

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Register = () => (
-  <div>Register</div>
-);
+const Register = () => {
+  const [credentials, setCredentials] = useState({
+    email: '',
+    password: '',
+    confirmPassword: ''
+  });
+
+  const onChange = e =>
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value
+    });
+
+  const submitForm = e => {
+    e.preventDefault();
+
+    console.log(credentials, 'credentials')
+  };
+
+  return (
+    <div>
+      <h1>
+        Register
+      </h1>
+      <form onSubmit={submitForm}>
+        <input type="email" name='email' onChange={onChange}/>
+        <input type="password" name='password' onChange={onChange}/>
+        <input type="password" name='confirmPassword' onChange={onChange}/>
+        <button>Register</button>
+      </form>
+    </div>
+  );
+};
 
 export default Register;
