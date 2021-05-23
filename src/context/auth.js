3b7562ff505f50ef0ae6d55dd8277b57cb7ca7b4
keithchasen <keithchasen89@gmail.com>
2021-05-24 01:19:35 +0300

    auth provider

--- src/context/auth.js
+++ src/context/auth.js
@@ -6,4 +6,44 @@ const AuthContext = createContext({
   logout: () => {}
 });
 
-export default AuthContext;
+const authReducer = (state, action) => {
+  switch (action.type) {
+    case 'LOGIN':
+      return {
+        ...state,
+        user: action.payload
+      };
+    case 'LOGOUT':
+      return {
+        ...state,
+        user: null
+      };
+    default:
+      return state;
+  }
+};
+
+const AuthProvider = props => {
+  const [state, dispatch] = useReducer(authReducer, {
+    user: null
+  });
+
+  const login = userData => {
+    dispatch({
+      type: 'LOGIN',
+      payload: userData
+    });
+  };
+
+  const logout = () => {
+    dispatch({
+      type: 'LOGOUT'
+    });
+  }
+
+  return (
+    <AuthContext.Provider value={{ user: state.user, login, logout}} {...props} />
+  );
+};
+
+export  { AuthContext, AuthProvider };
