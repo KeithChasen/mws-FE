24b603e8a2486410c2c261a3d1b0199c9c7ddd37
keithchasen <keithchasen89@gmail.com>
2021-05-24 18:34:36 +0300

    auth items in navbar depending on authenticated user

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import { AuthContext } from "../context/auth";
 
 const Menu = () => {
+  const { user, logout } = useContext(AuthContext);
   const activeStyle = { background: 'grey', color: 'black' };
   const styles = { margin: '10px' };
 
-  return (<nav>
-      <ul>
-        <NavLink exact to="/" activeStyle={activeStyle} style={styles}>Home</NavLink>
+  const authLinks = user ? (
+    <Link activeStyle={activeStyle} style={styles} onClick={logout}>Logout</Link>
+    ) : (
+      <>
         <NavLink to="/login" activeStyle={activeStyle} style={styles}>Login</NavLink>
         <NavLink to="/register" activeStyle={activeStyle} style={styles}>Register</NavLink>
+      </>
+  );
+
+  return (
+    <nav>
+      <ul>
+        <NavLink exact to="/" activeStyle={activeStyle} style={styles}>Home</NavLink>
+        { authLinks }
       </ul>
     </nav>
   )
