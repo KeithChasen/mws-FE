074bec166d8193002b4477639bdd862e9775911f
keithchasen <keithchasen89@gmail.com>
2021-06-07 21:51:53 +0300

    updated styled link text color

--- src/elements/StyledLink.js
+++ src/elements/StyledLink.js
@@ -3,16 +3,15 @@ import { NavLink } from "react-router-dom";
 
 const StyledLink = styled(NavLink)`
   margin: 10px;
-  color: ${props => props.theme.white};
+  color: ${props => props.theme.black};
+  ${({ nav }) => nav && css`color: ${props => props.theme.white}`};
   text-decoration: none;
   
   &:hover {
-  color: ${props => props.theme.black};
-  
-  ${({ nav }) => nav && 
-  css`background:  -webkit-linear-gradient(${props => props.theme.white}, ${props => props.theme.black});
+    background: -webkit-linear-gradient(${props => props.theme.white}, ${props => props.theme.black});
     -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;`
-  }`;
+    -webkit-text-fill-color: transparent;
+  }
+`;
 
 export default StyledLink;
