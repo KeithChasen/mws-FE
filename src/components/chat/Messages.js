b36ba8efffc8d54b7a4ffad7a536945522a1cb47
keithchasen <keithchasen89@gmail.com>
2021-06-22 21:05:42 +0300

    chat page markup and styles

--- src/components/chat/Messages.js
+++ src/components/chat/Messages.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useLazyQuery } from "@apollo/client";
 
-import { ChatMessagesHolder } from "../../elements/chat/wrapper";
+import { ChatMessagesHolder, ChatMessagesSpace, ChatInput, ChatButton } from "../../elements/chat/wrapper";
 import { GET_MESSAGES } from "../../graphql/chat";
 import { useUserState } from "../../context/user";
 
@@ -13,18 +13,38 @@ const Messages = () => {
     getMessages({ variables: { from: selectedUser }})
   }, [selectedUser]);
 
+  const sendMessage = e => {
+    e.preventDefault();
+  };
+
+  const getChatMarkup = message => (
+    <>
+      <ChatMessagesSpace>
+        { message }
+      </ChatMessagesSpace>
+      <form onSubmit={sendMessage}>
+        <ChatInput/>
+        <ChatButton>Send</ChatButton>
+      </form>
+    </>
+  );
+
   const getContent = () => {
     if (loading)
       return <div>Loading...</div>;
 
+    if(!data) {
+      return <div>Select someone to talk to...</div>
+    }
+
     if (!data.getMessages.length) {
       const user = users.find(user => user.id === selectedUser);
       const userLabel = user.nickname ?? user.id;
-      return <div>Start your conversation with {userLabel}</div>;
+      return getChatMarkup(`Start your conversation with ${userLabel}`);
     }
 
     if (data.getMessages.length)
-      return <div>Messages</div>
+      return getChatMarkup('');
   };
 
   return (
--- src/elements/chat/wrapper.js
+++ src/elements/chat/wrapper.js
@@ -8,7 +8,7 @@ const ChatWrapper = styled.div`
     );
     display: flex;
     justify-content: center;
-    height: 100vh;
+    min-height: 100vh;
 `;
 
 const ChatItemWrapper = styled.div`
@@ -25,8 +25,10 @@ const ChatUserList = styled.ul`
 `;
 
 const ChatUserListItem = styled.li`
-  border: 2px solid ${props => `${props.selected ? props.theme.white : props.theme.grey}`};
-  border-radius: 2vw;
+  border-top: 2px solid ${props => `${props.selected ? props.theme.white : props.theme.grey}`};
+  border-left: 2px solid ${props => `${props.selected ? props.theme.white : props.theme.grey}`};
+  border-bottom: 2px solid ${props => `${props.selected ? props.theme.white : props.theme.grey}`};
+  border-top-left-radius: ${props => props.selected ? `1vw` : 'none'} ;
   padding: 0.8vw;
   margin-bottom: 1vh;
   background: ${props => `${props.selected ? props.theme.grey : props.theme.white}`};
@@ -34,7 +36,46 @@ const ChatUserListItem = styled.li`
 `;
 
 const ChatMessagesHolder = styled.div`
-
+  margin-top: 1vh;
+  width: 100%;
+  min-height: 100vh;
+  background: ${props => props.theme.grey};
 `;
 
-export { ChatWrapper, ChatItemWrapper, ChatUserListWrapper, ChatUserList, ChatUserListItem, ChatMessagesHolder };
+const ChatMessagesSpace = styled.div`
+  height: 80vh;
+`;
+
+const inputStyles = css`
+  border: 0.2rem solid ${props => props.theme.grey};
+  display: inline-block;
+  border-radius: 0.5rem;
+  font-size: 1.7rem;
+  &:focus {
+     outline-color: ${(props) => props.theme.grey};
+   }
+`;
+
+const ChatInput = styled.input`
+  width: 80%;
+  margin-left: 5%;
+  ${inputStyles}
+`;
+
+const ChatButton = styled.button`
+  width: 10%;
+  margin-left: 0.1rem;
+  ${inputStyles}
+`;
+
+export {
+      ChatWrapper,
+      ChatItemWrapper,
+      ChatUserListWrapper,
+      ChatUserList,
+      ChatUserListItem,
+      ChatMessagesHolder,
+      ChatMessagesSpace,
+      ChatInput,
+      ChatButton
+};
