58512e4854da737529ad4b19ad037af6c46d6651
keithchasen <keithchasen89@gmail.com>
2021-06-22 11:31:52 +0300

    refactored users list component to be using user context

--- src/components/users/UsersList.js
+++ src/components/users/UsersList.js
@@ -1,33 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useQuery } from "@apollo/client";
 import { GET_USERS } from '../../graphql/users'
 
 import { UserListWrapper, UserList, UserLink } from "../../elements/users/list";
+import {useUserDispatch, useUserState} from "../../context/user";
 
 const UsersList = () => {
-  const { loading, error, data } = useQuery(GET_USERS);
+  const dispatch = useUserDispatch();
+  const { users } = useUserState();
+  const [error, setError] = useState(null);
 
-  const usersList = data?.getUsers &&
-    data.getUsers.map(
-      user => (
+  const { loading } = useQuery(GET_USERS, {
+    onCompleted: data => dispatch({ type: 'SET_USERS', payload: data.getUsers }),
+    onError: error => {
+      console.log(error);
+      setError('Error occured')
+    }
+  });
+
+  const getContent = () => {
+    if (loading)
+      return (<div>Loading...</div>);
+
+    if (error)
+      return (<div>{error}</div>);
+
+    if(!users || !users.length)
+      return (<div>No users...</div>);
+
+    else
+      return users.map(user => (
         <UserLink key={user.id} to={ `/user/${user.id}` }>
           { user.nickname ?? user.id  }
         </UserLink>
-      )
-    );
-
-  const content = loading ?
-    (<div>Loading...</div>) :
-      error ? (<div>Error: Wasn't able to fetch users list</div>) :
-        (
-          <UserList>
-            { usersList.map(user => user) }
-          </UserList>
-        );
+      ));
+  };
 
   return (
     <UserListWrapper>
-      { content }
+      <UserList>
+      { getContent() }
+      </UserList>
     </UserListWrapper>
   );
 };
