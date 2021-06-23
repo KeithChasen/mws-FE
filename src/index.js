26c6e2c01299df064dfcccfcef7df39c8d674f73
keithchasen <keithchasen89@gmail.com>
2021-06-23 16:25:17 +0300

    set up web sockets connection

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, split } from '@apollo/client';
 import { createUploadLink } from "apollo-upload-client/public/index";
 import { ApolloProvider } from '@apollo/client/react';
 import { setContext } from "@apollo/client/link/context";
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { getMainDefinition } from "@apollo/client/utilities";
 
 import App from './App';
+
+const wsLink = new WebSocketLink({
+  uri: process.env.REACT_APP_WS_SERVER,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      Authorization: `Bearer ${localStorage.getItem('token')}`
+    }
+  }
+});
 
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem('token');
@@ -22,8 +34,15 @@ const httpLink = authLink.concat(createUploadLink({
   uri: process.env.REACT_APP_SERVER
 }));
 
+const splitLink = split(({ query }) => {
+  const definition = getMainDefinition(query);
+  return (
+    definition.kind === 'OperationDefinition' && definition.operation === 'subscription'
+  );
+}, wsLink, httpLink);
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: splitLink,
   cache: new InMemoryCache()
 });
 
